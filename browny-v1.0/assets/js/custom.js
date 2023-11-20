f81aa0eba5a19d5c02dc47a4873cafebeed92880
HD0303 <202219404@jbnu.ac.kr>
2023-11-20 21:29:02 +0900

    스크롤 기능 삭제

--- browny-v1.0/assets/js/custom.js
+++ browny-v1.0/assets/js/custom.js
@@ -17,23 +17,7 @@ $(document).ready(function(){
 5. welcome animation support
 ======================================*/
 
-    // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
-	
-	
+
 	// 2. Smooth Scroll spy
 		
 		$('.header-area').sticky({
@@ -41,19 +25,7 @@ $(document).ready(function(){
         });
 		
 		//=============
-
-		$('li.smooth-menu a').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
-		});
 		
-		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
-		});
 
 	// 3. Progress-bar
 	
